f46633a4ccb932b1d29fb54934e1d8250ec1ae70
ndljk <kovacevic.nedeljko@yahoo.com>
2022-01-04 01:27:41 +0100

    initial commit

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button v-on:click="onClick()" class="btn" :style="{background:color}">{{text}}</button>
+</template>
+
+<script>
+
+export default {
+    name: 'Button',
+    props:{
+        text:String,
+        color:String,
+    },
+    methods:{
+        onClick(){
+            this.$emit('btn-click')
+        },
+    }
+}
+</script>
